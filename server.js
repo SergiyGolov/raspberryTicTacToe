2b2c6ac9d2c62bb0a23cf4136acbafaa3dd0ff05
SergiyGolov <sergiy.golov@hotmail.com>
2019-01-09 16:38:08 +0100

    attempt to get ngrok tunnel adress

--- server.js
+++ server.js
@@ -2,23 +2,12 @@ var http = require('http');
 
 var fs = require('fs');
 
-var exec = require('child_process');
+const { execFileSync } = require('child_process');
 
-exec("ngrok http 8080", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-});
+var ngrokAdress=execFileSync("getNgrokAdress.sh").toString();
+//to-do: find how to get running ngrok tunnel adress, put it in a variable, pass it to game.html and use a js template engine to io.connect to this adress
 
-exec("curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url\":\"https:..([^\"]*).*/\1/p'", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`); //TO-DO: use js templating engine to pass this variable to game.html and io.connect from game.html afterwards with this variable
-    console.log(`stderr: ${stderr}`);
-});
+console.log(ngrokAdress);
 
 var server = http.createServer((req, res) => {
     fs.readFile('./game.html', 'utf-8', function (error, content) {
