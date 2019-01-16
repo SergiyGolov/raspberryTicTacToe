efb2ac440c10779e93352893e31f2f25bb29442a
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-01-16 15:33:46 +0100

    added disconnect handling server and client side

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const { execFileSync } = require('child_process');
+const {
+    execFileSync
+} = require('child_process');
 
-var ngrokAdress=execFileSync("./getNgrokAdress.sh").toString().replace(/\s/g,'');
+var ngrokAdress = execFileSync("./getNgrokAdress.sh").toString().replace(/\s/g, '');
 
 var express = require('express');
 
@@ -13,15 +15,22 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 
-app.get('/', function(req, res) {
-    res.render('game.ejs',{serverAdress:ngrokAdress});
+app.get('/', function (req, res) {
+    res.render('game.ejs', {
+        serverAdress: ngrokAdress
+    });
 });
 
 
 io.sockets.on('connection', socket => {
-
     socket.on('new_user', nickName => {
         console.log(nickName + " connected to server!");
+        socket.nickname = nickName;
+    });
+
+    socket.on('disconnect', () => {
+        io.broadcast.emit('message',`${socket.nickname} disconnected`);
+        console.log(`${socket.nickname} disconnected`);
     });
 
 });
