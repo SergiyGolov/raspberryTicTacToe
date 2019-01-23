a7d53b25989728d39b0f3fa98d9a385efc1fcc2d
Sergiy Goloviatinski <sergiy.golov@hotmail.com>
2019-01-23 16:30:09 +0100

    more server side checking for tic tac toe

--- server.js
+++ server.js
@@ -14,41 +14,58 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
-
 app.get('/', function (req, res) {
     res.render('game.ejs', {
         serverAdress: ngrokAdress
     });
 });
 
+var players = new Array();
 
+var board = new Array(3);
 
-var playerCounter=0;
-
-
+for (var i = 0; i < board.length; i++) {
+    board[i] = new Array(3);
+    for (var j = 0; j < board[i].length; j++) {
+        board[i][j] = "white";
+    }
+}
 
 io.sockets.on('connection', socket => {
-    socket.on('new_user', nickName => {
-        console.log(nickName + " connected to server!");
-        socket.nickname = nickName;
-        playerCounter++;
-        if(playerCounter==1)
-        {
-            socket.emit("setColor","red");
-        }else if(playerCounter==2)
-        {
-            socket.emit("setColor","green");
-        }else{
-            socket.emit("setColor","white");
-            socket.emit("message","sorry, there are already 2 other players");
+    if (players.length == 0) {
+        socket.emit("setColor", "red");
+        socket.color = "red";
+        players.push(socket);
+        socket.emit("wait");
+    } else if (players.length == 1) {
+        if (players[0].color == "red") {
+            socket.emit("setColor", "green");
+            socket.color = "green";
+        } else {
+            socket.emit("setColor", "red");
+            socket.color = "red";
         }
-        console.log(playerCounter);
-    });
+        players.push(socket);
+        players[0].emit("stopWait");
+    } else {
+        socket.emit("setColor", "white");
+        socket.emit("message", "sorry, there are already 2 other players");
+    }
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message',`${socket.nickname} disconnected`);
-        console.log(`${socket.nickname} disconnected`);
-        playerCounter--;
+        let index = players.indexOf(socket);
+        if (index !== -1) {
+            players.splice(index, 1);
+        }
+    });
+
+    socket.on('play', (move) => {
+        if (board[move.x][move.y] == "white" && players.length == 2) {
+            board[move.x][move.y] = move.color;
+            players.forEach(player => {
+                player.emit('playOk', move);
+            });
+        }
     });
 
 });
